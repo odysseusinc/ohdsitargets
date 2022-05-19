81b292d2b6f72d59218a292498f99228642ba88f
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2022-05-19 08:26:20 -0400

    add diagnostic functions

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -1,13 +1,13 @@
 # TODO translate Cohort Diagnostics into targets workflow
 
-createDatabaseMeta <- function(connectionDetails,
-                               cdmDatabaseSchema,
-                               vocabularyDatabaseSchema,
-                               exportFolder,
-                               databaseId,
-                               databaseName = databaseId,
-                               databaseDescription = databaseId,
-                               minCellCount) {
+diagnosticsDatabaseMeta <- function(connectionDetails,
+                                    cdmDatabaseSchema,
+                                    vocabularyDatabaseSchema,
+                                    exportFolder,
+                                    databaseId,
+                                    databaseName = databaseId,
+                                    databaseDescription = databaseId,
+                                    minCellCount) {
   
   #connect to database
   connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
@@ -36,4 +36,44 @@ createDatabaseMeta <- function(connectionDetails,
   path <- file.path(exportFolder, "database.csv")
   return(path)
   
+}
+
+getObservationPeriod <- function(connectionDetails,
+                                 tempEmulationSchema = NULL,
+                                 cdmDatabaseSchema){
+  
+  
+  #connect to database
+  connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
+  on.exit(DatabaseConnector::disconnect(connection))
+  
+  observationPeriodDateRange <- DatabaseConnector::renderTranslateQuerySql(
+    connection = connection,
+    sql = "SELECT MIN(observation_period_start_date) observation_period_min_date,
+             MAX(observation_period_end_date) observation_period_max_date,
+             COUNT(distinct person_id) persons,
+             COUNT(person_id) records,
+             SUM(DATEDIFF(dd, observation_period_start_date, observation_period_end_date)) person_days
+             FROM @cdm_database_schema.observation_period;",
+    cdm_database_schema = cdmDatabaseSchema,
+    snakeCaseToCamelCase = TRUE,
+    tempEmulationSchema = tempEmulationSchema
+  )
+  return(observationPeriodDateRange)
+}
+
+
+diagnostics_ComputeCohortCounts <- function(connectionDetails,
+                                            cohortDatabaseSchema,
+                                            cohortTable,
+                                            cohorts, databaseId) {
+  
+  cohortCounts <- CohortDiagnostics:::getCohortCounts(
+    connectionDetails = connectionDetails,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    cohortIds = cohorts$cohortId
+  ) %>%
+    mutate(databaseId = databaseId)
+  return(cohortCounts)
 }
