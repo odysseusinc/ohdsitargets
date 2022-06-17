8a530dada44c5886717ca409c2838525a3f1fe4b
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2022-06-17 09:37:59 -0400

    remove old ohdsi project function

--- R/ohdsi_project.R
+++ R/ohdsi_project.R
@@ -1,53 +1,53 @@
-ohdsi_project <- function(path) {
-  # ensure path exists
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
-  
-  # create file structure  --------------------
-  #elaborate on file
-  fstr <- c(
-    paste0("input/", 
-           c(paste0("cohorts/", c("meta", "specification")), 
-             paste0("characterization/", c("meta", "specification")), 
-             paste0("estimation/" , c("meta", "specification")), 
-             paste0("prediction/" , c("meta", "specification"))
-           )
-    ),
-    paste0("output/", 
-           c(paste0("cohorts/", c("results")), 
-             paste0("diagnostics/", c("results")), 
-             paste0("characterization/", c("results")), 
-             paste0("estimation/", c("results")), 
-             paste0("prediction/", c("results"))
-           )
-    )
-  )
-  ff <- file.path(path, fstr)
-  purrr::walk(ff, ~dir.create(path = .x, recursive = TRUE, showWarnings = FALSE))
-  
-  
-  
-  #create .gitignore -------
-  git_ignores <-
-    c(
-      '.Rhistory',
-      '.Rapp.history',
-      '.RData',
-      '.Ruserdata',
-      '.Rproj.user/',
-      '.Renviron',
-      'config.yml'
-    )
-  writeLines(paste(git_ignores, sep = '\n'), con = file.path(path, ".gitignore"))
-  
-  #create config file --------------
-  #init_study(path)
-  
-  #create targets file --------------------
-  targetsPath <- system.file("targets/ohdsiScript.R", package = "ohdsitargets")
-  targetsFile <- file(targetsPath)
-  writeLines(readr::read_lines(targetsFile), con = file.path(path, "_targets.R"))
-  
-}
+# ohdsi_project <- function(path) {
+#   # ensure path exists
+#   dir.create(path, recursive = TRUE, showWarnings = FALSE)
+#   
+#   # create file structure  --------------------
+#   #elaborate on file
+#   fstr <- c(
+#     paste0("input/", 
+#            c(paste0("cohorts/", c("meta", "specification")), 
+#              paste0("characterization/", c("meta", "specification")), 
+#              paste0("estimation/" , c("meta", "specification")), 
+#              paste0("prediction/" , c("meta", "specification"))
+#            )
+#     ),
+#     paste0("output/", 
+#            c(paste0("cohorts/", c("results")), 
+#              paste0("diagnostics/", c("results")), 
+#              paste0("characterization/", c("results")), 
+#              paste0("estimation/", c("results")), 
+#              paste0("prediction/", c("results"))
+#            )
+#     )
+#   )
+#   ff <- file.path(path, fstr)
+#   purrr::walk(ff, ~dir.create(path = .x, recursive = TRUE, showWarnings = FALSE))
+#   
+#   
+#   
+#   #create .gitignore -------
+#   git_ignores <-
+#     c(
+#       '.Rhistory',
+#       '.Rapp.history',
+#       '.RData',
+#       '.Ruserdata',
+#       '.Rproj.user/',
+#       '.Renviron',
+#       'config.yml'
+#     )
+#   writeLines(paste(git_ignores, sep = '\n'), con = file.path(path, ".gitignore"))
+#   
+#   #create config file --------------
+#   #init_study(path)
+#   
+#   #create targets file --------------------
+#   targetsPath <- system.file("targets/ohdsiScript.R", package = "ohdsitargets")
+#   targetsFile <- file(targetsPath)
+#   writeLines(readr::read_lines(targetsFile), con = file.path(path, "_targets.R"))
+#   
+# }
 
 #' Create example ohdsitargets project
 #' 
